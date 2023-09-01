1dcbb88cfef9139aa034e72465c353a2d0f3039c
b2026ssokko <112681605+b2026ssokko@users.noreply.github.com>
2023-08-31 17:24:59 -0900

    Create Country.java

---
+++ Country.java
@@ -0,0 +1,16 @@
+public class Country {
+  String name = "Philippines";
+  int population = 0;
+  String continent = "";
+  
+  public static void main(String[] args) {
+    Country philippines = new Country();
+    System.out.println(philippines.name);
+  }
+}
+/*
+Countries
+- Name
+- Population
+- Continent
+*/
